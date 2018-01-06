9a4aa88d3b4e4f29277f22fc026ce7226211fdd0
David Humeau <davi881@gmail.com>
2018-01-06 11:44:33 -0400

    hoy

--- src/Pangram.hs
+++ src/Pangram.hs
@@ -1,2 +1,12 @@
 module Pangram where
 
+-- You will need to handle the case where a character in the sentence may be in
+-- uppercase, or may be a punctuation. `toLower` and `isAlpha` should help you with that.
+--
+import Data.Char (toLower, isAlpha)
+
+charPresent :: Char -> String -> Bool
+charPresent needle haystack = undefined
+
+isPangram :: String -> String -> Bool
+isPangram remainingChars remainingString = undefined
