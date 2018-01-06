431b5c5e4e9df8440495dc065a728d9a48c0235a
David Humeau <davi881@gmail.com>
2018-01-06 11:45:04 -0400

    hoy2

--- src/EvenList.hs
+++ src/EvenList.hs
@@ -7,3 +7,9 @@ evenList n lst
   | n == 0 = lst
   | even (n + n) = evenList (n - 1) ((n + n):lst)
   | otherwise = evenList (n - 1) lst
+
+evenList' :: Int -> [Int] -> [Int]
+evenList' n lst
+  | length lst == n = lst
+  | even (n + n) = evenList (n - 1) ((n + n):lst)
+  | otherwise = evenList (n - 1) lst
